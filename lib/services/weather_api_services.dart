aecd09c2bb71610e1183f11a748c94efca4e308e
h.moadeb <hamdimoadeb@gmail.com>
2023-11-29 12:05:36 +0100

    weather api services

---
+++ lib/services/weather_api_services.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/constants/constants.dart';
+import 'package:weather_app/exceptions/weather_exceptions.dart';
+import 'package:weather_app/models/weather.dart';
+import 'package:weather_app/services/http_error_handler.dart';
+
+import '../models/direct_geocoding.dart';
+
+class WeatherApiServices {
+  final http.Client httpClient;
+
+  const WeatherApiServices({
+    required this.httpClient,
+  });
+
+  Future<DirectGeocoding> getDirectGeocoding(String city) async {
+    final Uri uri = Uri(
+      scheme: 'https',
+      host: kApiHost,
+      path: '/geo/1.0/direct',
+      queryParameters: {
+        'q': city,
+        'limit': kLimit,
+        'appid': dotenv.env['APPID'],
+      },
+    );
+
+    try {
+      final http.Response response = await httpClient.get(uri);
+      if (response.statusCode != 200) {
+        throw httpErrorHandler(response);
+      }
+
+      final responseBody = json.decode(response.body);
+      if (responseBody.isEmpty) {
+        throw WeatherException('Connot get the location of $city');
+      }
+
+      final directGeocoding = DirectGeocoding.fromJson(responseBody);
+      return directGeocoding;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+
+  Future<Weather> getWeather(DirectGeocoding directGeocoding) async {
+    final Uri uri = Uri(
+      scheme: 'https',
+      host: kApiHost,
+      path: '/data/2.5/weather',
+      queryParameters: {
+        'lat': '${directGeocoding.lat}',
+        'lon': '${directGeocoding.lon}',
+        'units': kUnit,
+        'appid': dotenv.env['APPID'],
+      },
+    );
+
+    try {
+      final http.Response response = await httpClient.get(uri);
+      if (response.statusCode != 200) {
+        throw httpErrorHandler(response);
+      }
+
+      final responseBody = json.decode(response.body);
+      final Weather weather = Weather.fromJson(responseBody);
+      return weather;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+}
