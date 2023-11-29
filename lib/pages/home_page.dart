5803c1469193fa93c479168707534b7517ba2473
h.moadeb <hamdimoadeb@gmail.com>
2023-11-29 15:46:25 +0100

    show temperature data in HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recase/recase.dart';
+import 'package:weather_app/constants/constants.dart';
 import 'package:weather_app/cubits/weather/weather_cubit.dart';
 import 'package:weather_app/pages/search_page.dart';
 import 'package:weather_app/widgets/error_dialog.dart';
@@ -45,8 +47,35 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  String showTemperature(double temp) {
+    return temp.toStringAsFixed(2) + ' ℃';
+  }
+
+  Widget showIcon(String icon) {
+    return FadeInImage.assetNetwork(
+      placeholder: 'assets/images/loading.gif',
+      image: 'http://$kIconHost/img/wn/$icon@4x.png',
+      width: 96,
+      height: 96,
+    );
+  }
+
+  Widget formatText(String description) {
+    final formattedString = description.titleCase;
+    return Text(
+      formattedString,
+      style: const TextStyle(fontSize: 24.0),
+      textAlign: TextAlign.center,
+    );
+  }
+
   Widget showWeather() {
     return BlocConsumer<WeatherCubit, WeatherState>(
+      listener: (context, state) {
+        if (state.status == WeatherStatus.error) {
+          errorDialog(context, state.error.errMsg);
+        }
+      },
       builder: (context, state) {
         if (state.status == WeatherStatus.initial) {
           return const Center(
@@ -67,17 +96,77 @@ class _HomePageState extends State<HomePage> {
             ),
           );
         }
-        return Center(
-          child: Text(
-            state.weather.name,
-            style: TextStyle(fontSize: 18.0),
-          ),
+        return ListView(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Text(
+              state.weather.name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  TimeOfDay.fromDateTime(state.weather.lastUpdated)
+                      .format(context),
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '(${state.weather.country})',
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  showTemperature(state.weather.temp),
+                  style: const TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(width: 20.0),
+                Column(
+                  children: [
+                    Text(
+                      showTemperature(state.weather.tempMax),
+                      style: const TextStyle(fontSize: 16.0),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      showTemperature(state.weather.tempMin),
+                      style: const TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 40.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Spacer(),
+                showIcon(state.weather.icon),
+                Expanded(
+                  flex: 4,
+                  child: formatText(state.weather.description),
+                ),
+                Spacer(),
+              ],
+            )
+          ],
         );
-      },
-      listener: (context, state) {
-        if (state.status == WeatherStatus.error) {
-          errorDialog(context, state.error.errMsg);
-        }
       },
     );
   }
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatefulWidget {
+class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
-  @override
-  State<SettingsPage> createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.teal,
         title: Text('Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+
       ),
     );
   }
